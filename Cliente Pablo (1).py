9d48398db24d3f680f4164d3e083e89d9547dfb9
PabloLorca98 <80839316+PabloLorca98@users.noreply.github.com>
2025-04-21 12:35:40 -0400

    Add files via upload

---
+++ Cliente Pablo (1).py
@@ -0,0 +1,47 @@
+import socket
+import threading
+
+class ChatClient:
+    def __init__(self, host='127.0.0.1', port=55555):
+        self.host = host
+        self.port = port
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((host, port))
+        self.nickname = None
+
+    def receive(self):
+        while True:
+            try:
+                message = self.client.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.client.send(self.nickname.encode('utf-8'))
+                else:
+                    print(message)
+            except:
+                print("Error al recibir el mensaje!")
+                self.client.close()
+                break
+
+    def write(self):
+        while True:
+            message = input("")  # Espera la entrada del usuario
+            if message == 'exit':
+                self.client.send('exit'.encode('utf-8'))
+                self.client.close()
+                break
+            else:
+                message = f'{self.nickname}: {message}'
+                self.client.send(message.encode('utf-8'))
+
+    def start(self):
+        self.nickname = input("Elige un nickname: ")
+        
+        receive_thread = threading.Thread(target=self.receive)
+        receive_thread.start()
+        
+        write_thread = threading.Thread(target=self.write)
+        write_thread.start()
+
+if __name__ == "__main__":
+    client = ChatClient()
+    client.start()
